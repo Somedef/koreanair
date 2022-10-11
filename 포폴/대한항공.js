806d98fa868b76bc6fd9d6df856f4c1d5e5c276f
Somedef <osh0082@naver.com>
2022-10-11 01:22:04 +0000

    스크립트파일추가

---
+++ 포폴/대한항공.js
@@ -0,0 +1,42 @@
+
+
+$(function(){
+    $('.mainslide').slick();
+});
+// 메인슬라이드
+
+$('.es').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+// 이벤트슬라이드
+
+
+var header = document.querySelector('header'),
+    nav = document.querySelector('nav');
+    
+    nav.addEventListener('mouseover',function(){
+        header.style.height = '400px';
+    });
+    nav.addEventListener('mouseout',function(){
+        header.style.height = '90px';
+    });
+// 하위메뉴    
+
+$(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() >=300){
+            $('.go-top').fadeIn();
+        }else{
+            $('.go-top').fadeOut();
+        }
+    });
+
+    $('go-top').click(function(e){
+        e.preventDefault();
+        $('html, body').stop().animate({scrollTop:0},100)
+    });
+})
+// 탑버튼
